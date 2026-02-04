fb53fb9cb5d6e0f8011aac99430eef1c8c0d8c9f
Rajat Maurya <rajatmaurya176@gmail.com>
2026-02-05 01:48:00 +0530

    Portfolio added in Projects section

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -9,7 +9,13 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+
+        {/* <Card.Title>{props.title}</Card.Title> */}
+
+        <Card.Title className="project-title">
+          {props.title}
+        </Card.Title>
+
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
@@ -20,7 +26,7 @@ function ProjectCards(props) {
         {"\n"}
         {"\n"}
 
-      
+
 
         {!props.isBlog && props.demoLink && (
           <Button
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -21,14 +17,27 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      
+
+         
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
               title="Next-Gen AI Blogging Platform"
-              description = "Developing an AI-powered full-stack blog platform using the MERN stack that enables one-click blog summarization and AI-driven content/report generation for admins. Implements secure OTP and Google authentication, protected routes, and scalable REST APIs. Focused on delivering a responsive, production-ready application that enhances content creation efficiency and overall user experience."
+              description = "Developing an AI-powered full-stack blog platform using the MERN stack that enables one-click blog summarization and AI-driven content/report generation for admins. Implements secure OTP and Google authentication, protected routes, and scalable REST APIs. Focused on delivering a responsive, production-ready application."
+
+              ghLink="https://github.com/rajatmaurya-cs/Postify-AI-Powered-Blogging-Platform"
+              
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="portfolio"
+              description = "Developed a responsive personal portfolio using React, JavaScript, HTML, and CSS to showcase projects, skills, and achievements. Implemented reusable components, intuitive navigation, and performance optimization. Deployed a production-ready website with integrated GitHub, LinkedIn, and resume access to enhance recruiter engagement."
 
               ghLink="https://github.com/rajatmaurya-cs/Postify-AI-Powered-Blogging-Platform"
               
